42ac35ca070c68b465be6eff62ca17917e690325
daniel-pm19 <daniel.patino-m@mail.escuelaing.edu.co>
2025-09-23 16:21:58 -0500

    project created with springboot

---
+++ src/test/java/com/dosw/TallerEvaluativo2/TallerEvaluativo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dosw.TallerEvaluativo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TallerEvaluativo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
